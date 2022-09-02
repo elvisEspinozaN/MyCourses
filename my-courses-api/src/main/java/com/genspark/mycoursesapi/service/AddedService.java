31a3946ff321b6c60f5f0f91b6e41b01023193f1
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-09-02 14:48:29 -0400

    wip: addedServices initialized ( needs corrections )

--- my-courses-api/src/main/java/com/genspark/mycoursesapi/service/AddedService.java
+++ my-courses-api/src/main/java/com/genspark/mycoursesapi/service/AddedService.java
@@ -1,2 +1,16 @@
-package com.genspark.mycoursesapi.service;public interface AddedService {
+package com.genspark.mycoursesapi.service;
+
+import com.genspark.mycoursesapi.model.Added;
+import com.genspark.mycoursesapi.repository.projection.AddedItem;
+
+import java.util.List;
+
+/**
+ * @author elvisespinoza
+ */
+
+public interface AddedService {
+    Added saveAdded(Added added);
+
+    List<AddedItem> findAddedItemsOfUser(Long userId);
 }
--- my-courses-api/src/main/java/com/genspark/mycoursesapi/service/AddedServiceImpl.java
+++ my-courses-api/src/main/java/com/genspark/mycoursesapi/service/AddedServiceImpl.java
@@ -1,2 +1,34 @@
-package com.genspark.mycoursesapi.service;public class AddedServiceImpl {
+package com.genspark.mycoursesapi.service;
+
+import com.genspark.mycoursesapi.model.Added;
+import com.genspark.mycoursesapi.repository.AddedRepository;
+import com.genspark.mycoursesapi.repository.projection.AddedItem;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author elvisespinoza
+ */
+
+@Service
+public class AddedServiceImpl implements AddedService {
+    private final AddedRepository addedRepository;
+
+    public AddedServiceImpl(AddedRepository addedRepository) {
+        this.addedRepository = addedRepository;
+    }
+
+    @Override
+    public Added saveAdded(Added added) {
+        added.setAddedTime(LocalDateTime.now());
+        return addedRepository.save(added);
+    }
+
+    @Override
+    public List<AddedItem> findAddedItemsOfUser(Long userId) {
+        return addedRepository.findAllCoursesOfUser(userId);
+    }
+
 }
