66a321a2db26cf4dd08641d52649211386f0a596
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-09-02 08:32:05 -0400

    wip: backend api intialized

---
+++ my-courses-api/src/main/java/com/genspark/mycoursesapi/MyCoursesApiApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.mycoursesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoursesApiApplication.class, args);
+	}
+
+}
---
+++ my-courses-api/src/test/java/com/genspark/mycoursesapi/MyCoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.mycoursesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
